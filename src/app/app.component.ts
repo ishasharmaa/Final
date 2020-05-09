03e6a80cf1277b76137747bf436bae297f43925d
Isha Sharma <isha.sharma01@student.csulb.edu>
2020-05-09 11:22:32 -0700

    fcommits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-@Component({
+function isEven(n) {
+  n = Number(n);
+  return n === 0 || !!(n && !(n%2));
+}
+
+
+/*@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+})*/
 export class AppComponent {
   title = 'finals';
 }
